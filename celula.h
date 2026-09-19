d9272b4b9b96ea463b799c05a96efe6b88f5b82b
danielmeschiari <73394143+odanielmeschiari@users.noreply.github.com>
2026-09-19 16:24:29 -0300

    Update celula.h

--- celula.h
+++ celula.h
@@ -3,14 +3,21 @@
 #ifndef CELULA_H
 #define CELULA_H
 
+// inclusão da TAD horário
 #include "horario.h"
 
+// declaração da célula
 typedef struct celula_ celula;
 
+// função que cria uma célula dados os dados
 celula *criar_celula(int prior, horario *horario, char *descricao);
+// função que apaga uma célula
 void apagar_celula(celula **x);
+// função que obtém uma prioridade
 int celula_get_prior(celula *x);
+// função que obtém um horário
 horario *celula_get_horario(celula *x);
+// função que imprime uma célula
 void imprimir_celula(celula *x);
 
 #endif
