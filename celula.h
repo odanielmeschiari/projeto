8f588687d8de5eeee4881665df45ad21907a1e18
danielmeschiari <73394143+odanielmeschiari@users.noreply.github.com>
2026-09-24 15:52:23 -0300

    Add functions to set priority and horario in celula

--- celula.h
+++ celula.h
@@ -15,8 +15,12 @@ celula *criar_celula(int prior, horario *horario, char *descricao);
 void apagar_celula(celula **x);
 // função que obtém uma prioridade
 int celula_get_prior(celula *x);
+// função que muda uma prioridade
+void celula_set_prior(celula *x, int nova);
 // função que obtém um horário
 horario *celula_get_horario(celula *x);
+// função que muda um horário
+void celula_set_horario(celula *x, horario *novo);
 // função que imprime uma célula
 void imprimir_celula(celula *x);
 
