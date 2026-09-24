1ec9def73d06c70ab63c5f4713f1fdebdb688c50
danielmeschiari <73394143+odanielmeschiari@users.noreply.github.com>
2026-09-24 15:53:00 -0300

    Fix function prototype for tempo_abs

--- horario.h
+++ horario.h
@@ -8,7 +8,7 @@
 typedef struct HORARIO horario;
 
 // funcao auxiliar que calcula o tempo absoluto
-int tempo_abs(horario *horario)
+int tempo_abs(horario *horario);
 // funcao que cria um horario
 horario *horario_criar(int hh, int mm, int ss);
 // funcao que apaga um horario
